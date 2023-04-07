7b770a002d80e7f6400119a9c4ede70b4aec2bb8
Duy Le <lehongduy.bk@gmail.com>
2023-04-07 09:07:41 +0700

    update code - send header

--- routes/api.js
+++ routes/api.js
@@ -10,6 +10,8 @@ var Book = require("../models/book");
 
 const bodyParser = require("body-parser");
 
+var request = require('request');
+
 // // parse requests of content-type - application/json
 router.use(bodyParser.json());
 
@@ -21,7 +23,7 @@ router.use(parser);
 router.post('/signup', async function (req, res) {
 
     if (!req.body.username || !req.body.password) {
-        return res.render('api/signup', {msg: 'Please pass username and password.'});
+        return res.render('api/signup', { msg: 'Please pass username and password.' });
     } else {
         var newUser = new User({
             username: req.body.username,
@@ -40,18 +42,44 @@ router.get('/signup', (req, res) => {
 
 // Login
 
-router.post('/signin', async function (req, res) {
+router.post('/signin', async function (req, res, next) {
 
-    let user = await User.findOne({username: req.body.username});
+    let user = await User.findOne({ username: req.body.username });
     if (!user) {
-        res.status(401).render('signin', {msg: 'Tài khoản không tồn tại.'});
+        res.status(401).render('signin', { msg: 'Tài khoản không tồn tại.' });
     } else {
         user.comparePassword(req.body.password, function (err, isMatch) {
             if (isMatch && !err) {
                 var token = jwt.sign(user.toJSON(), config.secret);
-                return res.header("authorization", token).redirect('/api/book');
+
+                request.get({
+                    headers: { 'Authorization': 'JWT ' + token }
+                    , url: 'http://localhost:3000/api/book'
+                }, function (error, response, body) {
+                    res.send(body);
+                });
+
+                // res.set({
+                //     'Authorization': 'JWT ' + token
+                // });
+
+                //res.header('Authorization', 'JWT ' + token);
+
+                // res.cookie("token", 'JWT ' + token, {
+                //     maxAge: 60000
+                // });
+
+                // res.header['Authorization'] = 'JWT ' + token;
+
+                // console.log('Header: ' + res.header['Authorization']);
+
+                // res.redirect('/api/book');
+
+                //next();
+
+                // return res.header({ "Authorization": token }).redirect('/api/book');
             } else {
-                res.status(401).render('signin', {msg: 'Mật khẩu không đúng.'});
+                res.status(401).render('signin', { msg: 'Mật khẩu không đúng.' });
             }
         });
     }
@@ -64,7 +92,29 @@ router.get('/signin', (req, res) => {
 
 // Get List Book
 
+router.get('/book', passport.authenticate('jwt', { session: false }), async function (req, res) {
+
+    console.log('Vao get api/book')
+    console.log(req.header['Authorization']);
+    var token = getToken(req.headers);
+
+    console.log(token);
+    if (token) {
+        let booklist = await Book.find();
+        res.render('bookList', {
+            booklist: booklist.map(booklist => booklist.toJSON())
+        });
+
+        console.log(booklist)
+    } else {
+        return res.status(403).send({ success: false, msg: 'Unauthorized 123.' });
+    }
+});
+
 router.post('/book', passport.authenticate('jwt', { session: false }), function (req, res) {
+
+    console.log('Vao post api/book')
+
     var token = getToken(req.headers);
     if (token) {
         console.log(req.body);
@@ -80,22 +130,12 @@ router.post('/book', passport.authenticate('jwt', { session: false }), function 
             res.json({ success: true, msg: 'Successful created new book.' });
         });
     } else {
-        return res.status(403).send({ success: false, msg: 'Unauthorized.' });
+        return res.status(403).send({ success: false, msg: 'Unauthorized 456.' });
     }
 });
 
 
-router.get('/book', passport.authenticate('jwt', { session: false }), async function (req, res) {
-    var token = getToken(req.headers);
-    if (token) {
-        let booklist = await Book.find();
-        res.render('bookList', {
-            booklist: booklist.map(booklist => booklist.toJSON())
-        });
-    } else {
-        return res.status(403).send({ success: false, msg: 'Unauthorized.' });
-    }
-});
+
 
 getToken = function (headers) {
     if (headers && headers.authorization) {
